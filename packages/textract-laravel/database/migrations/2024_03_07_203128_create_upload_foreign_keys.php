ef48b0ddd4579a2a09e5f823c6033cfdff9063e1
Sam Street <samstreet@null.net>
2024-03-12 18:20:18 +0000

    [TAPI-24] As a Developer I must fix the migration that links the foreign keys incorrectly in the database for uploads and upload content

--- packages/textract-laravel/database/migrations/2024_03_07_203128_create_upload_foreign_keys.php
+++ packages/textract-laravel/database/migrations/2024_03_07_203128_create_upload_foreign_keys.php
@@ -9,7 +9,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('upload_content', fn(Blueprint $table) => $table->foreign('upload_id')->references('id')->on('uploads'));
-        Schema::table('uploads', fn(Blueprint $table) => $table->foreign('id')->references('upload_id')->on('upload_content')->cascadeOnDelete());
     }
 
     public function down(): void
