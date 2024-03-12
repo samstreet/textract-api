bdea46628cd8e652421d9842c34ad2094abcddf3
Sam Street <samstreet@null.net>
2024-03-12 19:59:53 +0000

    [TAPI-9] As a Developer I must write the custom API endpoint to handle POST requests for uploading a document

---
+++ packages/textract-laravel/app/Http/Requests/TextractUploadRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use TextractAPI\Core\Http\Requests\Concerns;
+
+class TextractUploadRequest extends CoreRequest
+{
+    use Concerns\HasNoValidator;
+
+    public function rules(): array
+    {
+        return [
+            'pdf' => ['required', 'file']
+        ];
+    }
+
+    public function getFileAsBase64EncodedString(): string
+    {
+        return chunk_split(base64_encode(file_get_contents($this->file('pdf'))));
+    }
+}
